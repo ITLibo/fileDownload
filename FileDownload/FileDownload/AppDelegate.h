8f590d3890645f8f9d57cbb84537944f487b1f10
李博 <libo@libodeMac-mini.local>
2017-09-04 17:38:41 +0800

    //初始化项目

---
+++ FileDownload/FileDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FileDownload
+//
+//  Created by 李博 on 2017/9/4.
+//  Copyright © 2017年 李博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FileDownload/FileDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FileDownload
+//
+//  Created by 李博 on 2017/9/4.
+//  Copyright © 2017年 李博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
